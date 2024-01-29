f7bd15eda5a3ef99df8267144528488e4a802582
ZMP <zane.prunella@gmail.com>
2024-01-29 08:28:25 -0800

    Working with JSX slots, Setting Component Types Dynamically, Setting Default Prop Values.

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  console.log('TABBUTTON COMPONENT EXECUTING');
+export default function TabButton({ children, isSelected, ...props }) {
+  console.log("TABBUTTON COMPONENT EXECUTING");
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+      <button className={isSelected ? "active" : undefined} {...props}>
         {children}
       </button>
     </li>
